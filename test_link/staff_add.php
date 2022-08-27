3da48241e6e48db0ea59f8fd9f19dad39d206b39
nogak <n.mouse.work@gmail.com>
2022-08-27 05:54:31 +0000

    push_rasp_env

---
+++ test_link/staff_add.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ロクマル農園</title>
+</head>
+<body>
+
+スタッフの追加<br>
+<br>
+<form method="post" action="staff_add_check.php">
+スタッフ名を入力してください。<br>
+<input type="text" name="name" style="width:200px"><br>
+パスワードを入力してください。<br>
+<input type="password" name-"pass" style-"width:100px"><br>
+パスワードをもう一度入力してください。<br>
+<input type="password" name="pass2" style="width:100px"><br>
+<br>
+<input type="button" onclick="history.back()" value="戻る">
+<input type="submit" value="OK">
+</form>
+
+</body>
+</html>
